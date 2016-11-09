590d93ba1e6f814c27e44f1ba59939343a9dffc5
MalvinJu <malvinjuanda@yahoo.com>
2016-11-09 22:49:44 +0700

    manage file

---
+++ saleProject/src/java/IdentityService/access.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IdentityService;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.sql.*;
+
+/**
+ *
+ * @author Asus
+ */
+public class access {
+    private String username;
+    private String password;
+    private String token;
+    
+    public access (String user, String pass) {
+        username = user;
+        password = pass;
+    }
+    
+    public void setUsername (String user) {
+        username = user;
+    }
+    
+    public void setPassword (String pass) {
+        password = pass;
+    }
+    
+    public String getToken () {
+        Random random = new SecureRandom();
+        token = new BigInteger(130, random).toString(32);
+        
+        return token;
+    }
+    
+    public Boolean isAuthenticate () {
+        Boolean authenticate = false;
+        String query = "select * from user where username = \"" + username + "\" and password = \"" + password + "\"";
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/saleProject_user","root","malvin");  
+            Statement stmt=con.createStatement();  
+            ResultSet rs=stmt.executeQuery(query);  
+            if(rs.next()) {
+                authenticate = true;
+            }
+            con.close();  
+        }
+        catch(Exception e){ 
+            System.out.println(e);
+        }
+        
+        return authenticate;
+    }
+}
---
+++ saleProject/src/java/IdentityService/identityService.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IdentityService;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Asus
+ */
+public class identityService extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String user = request.getParameter("username");
+            String pass = request.getParameter("password");
+            access connect = new access (user, pass);
+            
+            //if request is not from HttpServletRequest, you should do a typecast before
+            HttpSession session = request.getSession();
+                
+            if (connect.isAuthenticate()) {
+                //save message in session
+                session.setAttribute("message", "Hello world");
+                
+                request.setAttribute("servletName", "pisang");
+                RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/saleProject/viewKatalog.jsp");
+                dispatcher.forward(request,response);
+                //response.sendRedirect("/saleProject/index.jsp");
+            }
+            else {
+                session.setAttribute("message", "error");
+                response.sendRedirect("/saleProject/index.jsp");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
